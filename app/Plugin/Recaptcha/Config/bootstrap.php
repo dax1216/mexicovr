85ec3395614368d7708b4864b07e12d9aa3c268d
Dax Panganiban <p_dax2003@yahoo.com>
2013-10-18 13:55:59 +0800

    Adding recaptcha plugin

---
+++ app/Plugin/Recaptcha/Config/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+Configure::write('Recaptcha.publicKey', '6LdZfOUSAAAAADVgzUXw2oG00ZNEs5QZGLmlZ4dq');
+Configure::write('Recaptcha.privateKey', '6LdZfOUSAAAAADTPPSVzs4tAN7R3ivWfB4yzDW45');
+?>
